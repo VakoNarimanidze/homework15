146a7b1ced29595e774c6a471236f569d1096350
VakoNarimanidze <119787310+VakoNarimanidze@users.noreply.github.com>
2023-01-23 22:39:44 +0400

    .

---
+++ homework15.js
@@ -0,0 +1,33 @@
+for (let i = 1; i <101; i++) {
+    console.log(i); 
+}
+// with for loop
+ 
+let item = 1;
+while (item < 51) {
+    console.log(item)
+     item++;  
+}
+// with while loop
+
+
+    let secondItem = 1;
+    do {
+        console.log(secondItem);
+        secondItem++;
+       } while (secondItem < 151); 
+
+       // with do/while loop
+  
+
+
+       let Array = ["George","Nia","Anna",'Alex','Nick'];
+       console.log(Array)
+       Array.unshift('David');
+       Array.push('Salome');
+       console.log(Array);
+       Array.shift();
+       Array.pop();
+       console.log(Array);
+
+
